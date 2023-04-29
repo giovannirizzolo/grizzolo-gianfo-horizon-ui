76f5b158c473be55404f9f9302d8222d9a66aad8
giovannirizzolo <rizzolo.giovanni98@gmail.com>
2023-04-29 20:47:05 +0200

    fix:develop remove useful component, add axios

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { ThemeEditorProvider } from "@hypertheme-editor/chakra-ui";
+import "assets/css/App.css";
+import AdminLayout from "layouts/admin";
+import AuthLayout from "layouts/auth";
+import RTLLayout from "layouts/rtl";
 import React from "react";
 import ReactDOM from "react-dom";
-import "assets/css/App.css";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
-import AuthLayout from "layouts/auth";
-import AdminLayout from "layouts/admin";
-import RTLLayout from "layouts/rtl";
-import { ChakraProvider } from "@chakra-ui/react";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import theme from "theme/theme";
-import { ThemeEditorProvider } from "@hypertheme-editor/chakra-ui";
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
