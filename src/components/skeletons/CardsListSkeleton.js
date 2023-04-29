014356ba93bffcb5a7bac682d036434e4f6e1c91
giovannirizzolo <rizzolo.giovanni98@gmail.com>
2023-04-29 20:44:24 +0200

    chore:feature/applications add application page, card and hooks

---
+++ src/components/skeletons/CardsListSkeleton.js
@@ -0,0 +1,33 @@
+import { Box, Divider, Flex, SimpleGrid, Skeleton, Spacer, Stack } from "@chakra-ui/react/dist/chakra-ui-react.cjs"
+import Card from "components/card/Card"
+
+const CardsListSkeleton = () => {
+    return(
+        <SimpleGrid
+            mb='20px'
+            columns={{ sm: 1, md: 4, '2xl': 4 }}
+            spacing={{ base: "20px", xl: "20px" }}
+        >
+            {Array.from({length: 16}, (_, index) => (
+                <Card
+                alignSelf='center'
+                w={{ base: "100%", xl: "99%" }}
+                h={{base: "100%"}}
+                mx='auto'
+                p='20px'>
+                <Flex direction='column' h='100%' w='100%'>
+                    <Stack >
+                        <Skeleton height='20px' />                        
+                        <Skeleton height='20px' />                        
+                        <Skeleton height='20px' />                        
+                    </Stack>
+                </Flex>
+              </Card>
+            ))}
+          
+        </SimpleGrid>
+        
+    )
+}
+
+export default CardsListSkeleton
